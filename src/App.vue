f579c1138228c9d42a49ed505ac0e693c450b521
Eli Paulsen <eli-paulsen@uiowa.edu>
2023-12-11 19:50:52 -0600

    complete button strikthrough styling feature

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,7 @@
   <main>
     <Banner/>
     <Task/>
+    <Task/>
   </main>
 </template>
 
--- src/components/Task.vue
+++ src/components/Task.vue
@@ -1,7 +1,9 @@
 <!-- Task.vue -->
 <template>
-  <div class="task">
-    <h2>{{ task }}</h2>
+  <div class="task" :class="{ completed: isCompleted }">
+    <h2 :class="{ completed: isCompleted }">{{ task }}</h2>
+    <button class="remBtn" @click="removeTask">remove</button>
+    <button class="cmpBtn" @click="markAsComplete">complete</button>
   </div>
 </template>
 
@@ -9,14 +11,30 @@
 export default {
   data () {
     return {
-      task: "this is a sample task"
+      isCompleted: false,
+      task: "this is a sample task to test a a a a"
     };
   },
+
+  methods: {
+    markAsComplete() {
+      this.isCompleted = true;
+      console.log("Task completed:", this.task);
+    },
+
+    removeTask() {
+      // Add logic here to handle task removal
+      console.log("Task removed:", this.task);
+      // You might want to update the task list or perform other actions
+    }
+  }
 };
 </script>
 
 <style scoped>
   .task {
+    margin-top: 50px;
+    display: flex;
     height: auto;
     margin-left: 250px;
     width: 500px;
@@ -29,6 +47,27 @@ export default {
     padding-top: 15px;
     padding-bottom: 15px;
     margin-left: 25px;
+    margin-right: 100px;
     color: lightgray;
   }
+
+  .remBtn {
+    border-color: red;
+    color: red;
+    background-color: rgb(35, 35, 35);
+    width: 100px;
+  }
+
+  .cmpBtn {
+    border-color: lime;
+    color: lime;
+    width: 100px;
+    background-color: rgb(35,35,35);
+  }
+
+  .completed h2 {
+    text-decoration: line-through;
+    color: rgb(72, 72, 72);
+  }
+
 </style>
