1d8e9595f3b56996b1b55d5b47ba471487d6aceb
Eli Paulsen <eli-paulsen@uiowa.edu>
2023-12-13 23:10:01 -0600

    vuetify support

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,30 @@
 <script setup>
   import Banner from './components/Banner.vue';
   import Task from './components/Task.vue';
+  import AddTask from './components/AddTask.vue';
 </script>
 
 <template>
-  <main>
+  <v-app class="app">
     <Banner/>
-    <Task/>
-    <Task/>
-  </main>
+
+    <!-- Sizes your content based upon application components -->
+    <v-main>
+      <!-- Provides the application the proper gutter -->
+      <v-container>
+        <AddTask/>
+        <v-divider></v-divider>
+        <Task/>
+      </v-container>
+    </v-main>
+
+  </v-app>
+
 </template>
 
 <style scoped>
+  .app {
+    background-image: linear-gradient(lightcyan, cadetblue);
+    font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
+  }
 </style>
